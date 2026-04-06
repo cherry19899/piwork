9970a794c84b10abc443fb561c991c49a8d163e2
cherry19899 <den.vishnja@mail.ru>
2026-04-06 17:18:35 +0200

    Fix Pi Browser iframe blocking

--- next.config.js
+++ next.config.js
@@ -15,4 +15,17 @@ const nextConfig = {
   },
 }
 
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.pinet.com https://*.minepi.com https://apppiworkcfacbda4498.pinet.com;",
+          },
+        ],
+      },
+    ];
+  },
 module.exports = nextConfig
